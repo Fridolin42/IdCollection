3d25bde8a3bd5c9ec2de7f3c8544990e2b2e2dfa
fridolin <@>
2026-06-19 11:54:02 +0200

    Additional speed test for IdMap

--- src/test/kotlin/de/fridolin1/idMap/IdMapSpeedTest.kt
+++ src/test/kotlin/de/fridolin1/idMap/IdMapSpeedTest.kt
@@ -1,47 +1,100 @@
 package de.fridolin1.idMap
 
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class IdMapSpeedTest {
-    @Test
-    fun speedTest() {
-        var sum1 = 0L
-        var sum2 = 0L
-        repeat(1.shl(12)) {
-            sum1 += measureIDMap()
-            sum2 += measureHashMap()
+    @Nested
+    inner class FillChangeAndEmptySpeedTest {
+        @Test
+        fun speedTestFillChangeAndEmpty() {
+            var sum1 = 0L
+            var sum2 = 0L
+            repeat(1.shl(12)) {
+                sum1 += measureIDMapFillChangeAndEmpty()
+                sum2 += measureHashMapFillChangeAndEmpty()
+            }
+            println("Speed Test 2: " + sum1 / sum2.toDouble())
+            assertTrue { sum1 < sum2 }
         }
-        println(sum1 / sum2.toDouble())
-        assertTrue { sum1 < sum2 }
+
+        fun measureIDMapFillChangeAndEmpty(): Long {
+            val start1 = System.nanoTime()
+            val idMap = IdMap<String>(1.shl(15))
+            repeat(1.shl(15)) {
+                idMap[it] = "Hello World $it"
+            }
+            for (id in idMap) {
+                idMap[id] = "$id changed"
+            }
+            for (id in idMap.toList()) {
+                assertEquals("$id changed", idMap[id])
+                idMap.remove(id)
+            }
+            assertEquals(0, idMap.size)
+            return System.nanoTime() - start1
+        }
+
+        fun measureHashMapFillChangeAndEmpty(): Long {
+            val start1 = System.nanoTime()
+            val hashMap = HashMap<Int, String>()
+            repeat(1.shl(15)) {
+                hashMap[it] = "Hello World $it"
+            }
+            for (id in hashMap.keys) {
+                hashMap[id] = "$id changed"
+            }
+            for (id in hashMap.keys.toList()) {
+                assertEquals("$id changed", hashMap[id])
+                hashMap.remove(id)
+            }
+            assertEquals(0, hashMap.size)
+            return System.nanoTime() - start1
+        }
     }
 
-    fun measureIDMap(): Long {
-        val start1 = System.nanoTime()
-        val idMap = IdMap<String>(1.shl(15))
-        repeat(1.shl(15)) {
-            idMap[it] = "Hello World $it"
+    @Nested
+    inner class FillAndEmptySpeedTest {
+        @Test
+        fun speedTestFillAndEmpty() {
+            var sum1 = 0L
+            var sum2 = 0L
+            repeat(1.shl(12)) {
+                sum1 += measureIDMapFillAndEmpty()
+                sum2 += measureHashMapFillAndEmpty()
+            }
+            println("Speed Test 1: " + sum1 / sum2.toDouble())
+            assertTrue { sum1 < sum2 }
         }
-        for (id in idMap.toList()) {
-            assertEquals("Hello World $id", idMap[id])
-            idMap.remove(id)
-        }
-        assertEquals(0, idMap.size)
-        return System.nanoTime() - start1
-    }
 
-    fun measureHashMap(): Long {
-        val start1 = System.nanoTime()
-        val hashMap = HashMap<Int, String>()
-        repeat(1.shl(15)) {
-            hashMap[it] = "Hello World $it"
+        fun measureIDMapFillAndEmpty(): Long {
+            val start1 = System.nanoTime()
+            val idMap = IdMap<String>(1.shl(15))
+            repeat(1.shl(15)) {
+                idMap[it] = "Hello World $it"
+            }
+            for (id in idMap.toList()) {
+                assertEquals("Hello World $id", idMap[id])
+                idMap.remove(id)
+            }
+            assertEquals(0, idMap.size)
+            return System.nanoTime() - start1
         }
-        for (id in hashMap.keys.toList()) {
-            assertEquals("Hello World $id", hashMap[id])
-            hashMap.remove(id)
+
+        fun measureHashMapFillAndEmpty(): Long {
+            val start1 = System.nanoTime()
+            val hashMap = HashMap<Int, String>()
+            repeat(1.shl(15)) {
+                hashMap[it] = "Hello World $it"
+            }
+            for (id in hashMap.keys.toList()) {
+                assertEquals("Hello World $id", hashMap[id])
+                hashMap.remove(id)
+            }
+            assertEquals(0, hashMap.size)
+            return System.nanoTime() - start1
         }
-        assertEquals(0, hashMap.size)
-        return System.nanoTime() - start1
     }
 }
